ab8ddf0bbef3102362481dca8519b9ece4322216
Federico Vitale <fedevitale99@gmail.com>
2018-04-25 15:24:17 +0200

    1.0.2

--- test.js
+++ test.js
@@ -3,7 +3,7 @@ import execa from 'execa'
 import pkg from './package'
 
 test('Version Check', t => {
-  execa('node', ['index.js', '--version'], (stderr, stdout) => {
+  execa('node', ['build/bin/index.js', '--version'], (stderr, stdout) => {
     t.is(stdout, pkg.version)
   })
 })
